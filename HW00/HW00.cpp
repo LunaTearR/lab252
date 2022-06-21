e0e5f62ff11e79bc064451f45ae9a408685b3c63
LunaTearR <saengtawanpoopoom@gmail.com>
2022-06-21 13:56:42 +0700

    first commit

---
+++ HW00/HW00.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class Employee {
+    
+private: // Private attribute
+    int salary;
+
+public:
+    // Setter
+    void setSalary(int s) {
+        salary = s;
+    }
+    
+    // Getter
+    int getSalary() {
+        return salary;
+    }
+};
